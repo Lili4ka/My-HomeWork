1b794d66c01dd1c4b6fe728dd2c60e2053f20558
Lil'ok Zubko <lilia.zubko97@gmail.com>
2016-02-23 23:14:19 +0200

    Update Shop.java

--- ProdShop/Shop.java
+++ ProdShop/Shop.java
@@ -1,1 +1,53 @@
+/**
+ * Created by Admin on 23.02.2016.
+ */
+public class Shop {
+    protected String name;
+    protected String address;
+    protected int numberProductOrder;
+    protected Product[] productArray;
+    protected int number;
+
+    public Shop() {
+        this.productArray = new Product[100];
+        this.number = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumberProductOrder() {
+        return number;
+    }
+
+    public void setNumberProductOrder(int numberProductOrder) {
+        this.numberProductOrder = numberProductOrder;
+    }
+
+    public void addProduct(Product product) {
+        this.productArray[this.number] = product;
+        this.number++;
+    }
+
+    public String toString() {
+        String result = "You need to order " + this.numberProductOrder + " product for shop " + this.name + " on address " + this.address + ":\n--------\n";
+        for(int i = 0; i < this.number; i++) {
+            result += this.productArray[i].toString() + "\n--------\n";
+        }
+        return result;
+    }
+}
 
