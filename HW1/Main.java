1ce68fbd87221e708bea026d2403cadebabc3c44
Zvon4u4ka Lilu <lilia.zubko97@gmail.com>
2016-05-18 08:53:43 +0300

    Update Main.java

--- HW1/Main.java
+++ HW1/Main.java
@@ -1,16 +1,19 @@
 package HW1;
 
-
 public class Main {
     public static void main(String[] args) {
         Product product = new Product();
 
         product.setType("Milk");
+        System.out.println("type:" + product.getType());
         product.setBrand("Prostokwasha");
+        System.out.println("brand:" + product.getBrand());
         product.setManufacturer("UK");
+        System.out.println("manufacturer:" + product.getManufacturer());
         product.setWeight(450);
+        System.out.println("weight:" + product.getWeight());
         product.setPrice(18);
-        product.setProductLife(5);
+        System.out.println("price:" + product.getPrice());
 
         System.out.println(product.toString());
 
